5d26192167a89061b1b668ffa81da72a3fa74258
Jose Victor Catalani <josevictorcatalani@gmail.com>
2021-12-13 00:02:26 -0300

    f: added support to scss

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import React from "react";
+import "../styles/globals.scss";
+import App from "../ui/common/App";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <App>
+      <Component {...pageProps} />
+    </App>
+  );
 }
 
-export default MyApp
+export default MyApp;
