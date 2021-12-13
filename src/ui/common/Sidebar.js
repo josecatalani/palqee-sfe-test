a45d5f6350d821df45bf78a29a2fef8f6107bb3b
Jose Victor Catalani <josevictorcatalani@gmail.com>
2021-12-13 00:26:39 -0300

    f: added fonts, and cleaned previous styles

--- src/ui/common/Sidebar.js
+++ src/ui/common/Sidebar.js
@@ -1,5 +1,58 @@
 import React from "react";
+import Link from "next/link";
+import styles from "../../styles/Sidebar.module.scss";
 
-export default ({ children }) => {
-  return <aside id="sidebar">{children}</aside>;
+const MenuItem = ({ label, link, children }) => (
+  <li className={styles.menuItem}>
+    {children ? (
+      <>
+        {label}
+        {children}
+      </>
+    ) : (
+      <Link href={link}>
+        <a>{label}</a>
+      </Link>
+    )}
+  </li>
+);
+
+const Sidebar = () => {
+  const menuList = [
+    {
+      label: "Home",
+      link: "/",
+    },
+    {
+      label: "StarWars",
+      children: [
+        {
+          label: "Characters",
+          link: "/characters",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <aside className={styles.sidebar}>
+      {menuList.map((menuItem) => {
+        const { children } = menuItem;
+        if (children) {
+          return (
+            <MenuItem {...menuItem}>
+              <ul>
+                {children.map((childrenItem) => (
+                  <MenuItem {...childrenItem} />
+                ))}
+              </ul>
+            </MenuItem>
+          );
+        }
+        return <MenuItem {...menuItem} />;
+      })}
+    </aside>
+  );
 };
+
+export default Sidebar;
